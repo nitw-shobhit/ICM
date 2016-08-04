d5704021ef3b70bb804b0d72e5a06be6f138f634
Shobhit Tyagi <shobhit.tyagi@accedo.tv>
2016-08-04 13:21:56 +0200

    Structure

---
+++ icm-web/src/main/java/show/icm/Runner.java
@@ -0,0 +1,12 @@
+package show.icm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Runner {
+
+    public static void main(final String[] args) {
+    	SpringApplication.run(Runner.class, args);
+    }
+}
---
+++ icm-web/src/main/resources/static/js/icmMain.js
@@ -0,0 +1,1 @@
+var module = angular.module('icm-app', ['ui.router', 'ui.bootstrap', 'ngDialog']);
